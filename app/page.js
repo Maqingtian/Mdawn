4f1ddd8907f7fd9e74af2b3094dbc38e6075ab0d
MaQingtian <2366363413@qq.com>
2025-05-15 16:06:32 +0800

    test

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default async function Page() {
           <BasisInfo />
           <SkillsBar />
           <Suspense fallback={<Skeleton />}>
-            <Spotify />
+            {/* <Spotify /> */}
           </Suspense>
         </aside>
       </section>
